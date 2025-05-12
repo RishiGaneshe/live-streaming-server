35aaf5980e0e82376bf8637944c75c70733a18af
Abhishek Ganeshe <abhishekganeshe33@gmail.com>
2025-05-12 17:44:37 +0530

    changed RTMP Server Configurations

--- configs/rtmp.config.js
+++ configs/rtmp.config.js
@@ -3,39 +3,39 @@ const { ffmpegPath, hlsPath, hlsPathLinux, ffmpegLinuxPath }= require('./domain.
 
 
 const config = {
-    rtmp: {
-      port: 1936,
-      chunk_size: 60000,
-      gop_cache: true,
-      ping: 30,
-      ping_timeout: 60
-    },
-    http: {
-      port: 5000,  
-      allow_origin: '*',
-      mediaroot: './media'
-    },
-    auth: {
-      play: false,     
-      publish: false, 
-      secret: 'supersecret' 
-    },
-    trans: {
-      ffmpeg: ffmpegPath,
-      tasks: [
-        {
-          app: 'live',
-          hls: true,
-          hlsFlags: '[hls_time=1:hls_list_size=2:hls_flags=delete_segments+omit_endlist]',
-          hlsKeep: true,
-          hlsPath: hlsPath,
-          vc: null,
-          ac: null,
-          rtmp: true,
-          hlsVariant: []
+        rtmp: {
+          port: 1936,
+          chunk_size: 60000,
+          gop_cache: true,
+          ping: 30,
+          ping_timeout: 60
+        },
+        http: {
+          port: 5000,  
+          allow_origin: '*',
+          mediaroot: './media'
+        },
+        auth: {
+          play: false,     
+          publish: false, 
+          secret: 'supersecret' 
+        },
+        trans: {
+          ffmpeg: ffmpegPath,
+          tasks: [
+            {
+              app: 'live',
+              hls: true,
+              hlsFlags: '[hls_time=1:hls_list_size=2:hls_flags=delete_segments+omit_endlist]',
+              hlsKeep: true,
+              hlsPath: hlsPath,
+              vc: null,
+              ac: null,
+              rtmp: true,
+              hlsVariant: []
+            }
+          ]
         }
-      ]
-    }
   }
 
 
